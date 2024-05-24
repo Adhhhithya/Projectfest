a5eb96116b42fe7b2977ff1eb2bf200be69eecf4
Israel Paul <130955174+I-paul@users.noreply.github.com>
2024-05-24 19:57:49 +0530

    done

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var checkbox = document.getElementsById('checkbox');
-var confirms = document.getElementsById('confirmed');
+const cb = document.getElementsByClassName('checkbox');
+const confirms = document.getElementsByClassName('confirmed');
 let count = 0;
-function checkme(){
-    if (checkbox.checked==true) {
+checkbox.addEventListener('change', function oncheck(){
+    if (cb.checked) {
         confirms.style.display = 'block';
     } else {
-        checkbox.checked=false;
+        cb.checked=false;
         // Checkbox is unchecked
         // You can add code here if you want to do something when the checkbox is unchecked
     }
-}
+});
